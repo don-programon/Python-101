68b4d9ba7c1cc653a0d59bb2d529fb4d14b30d35
don-programon <125381068+don-programon@users.noreply.github.com>
2023-02-16 17:46:17 +0100

    Add files via upload

---
+++ modulos.py
@@ -0,0 +1,16 @@
+help("modules")
+# Importar modulos
+
+#opcion 1
+import this as t
+print(t.d)
+
+#opcion 2
+import this
+print(this.c)
+print(this.s)
+
+#opcion 3
+from this import c,s
+print(c)
+print(s)
