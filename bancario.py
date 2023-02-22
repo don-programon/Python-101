866a43ebb093019255846e3c163f130ab30f2335
don-programon <125381068+don-programon@users.noreply.github.com>
2023-02-22 17:52:04 +0100

    Add files via upload

--- bancario.py
+++ bancario.py
@@ -1,27 +1,48 @@
-#PROGRAMA PARA LLEVAR CONTROL DE TRANSACCIONES
+#PROGRAMA PARA LLEVAR CONTROL DE TRANSACCIONES BANCARIAS
 saldo = float(input("Cual es tu saldo? "))
-transacciones = []
+media = 0
+lista1 = []
+conteo = 0
 
+#HACEMOS QUE EL PROGRAMA REPITA TRANSACCIONES MIENTRAS EL SALDO SEA MAS QUE 0
 while saldo > 0:
     transaccion = float(input("De cuanto es la transaccion? "))
     tipo = (input("Teclea R para retiro o I para hacer un ingreso: "))
 
+#FUNCION PARA RETIRAR DINERO
     if tipo == "r" or tipo == "R":
         saldo = saldo - transaccion
-        transacciones.append([transaccion, tipo])
+        conteo = conteo + 1
+        tipo = "- Retiro"
+        lista1.append([- transaccion, tipo])
         print(f"Tu saldo actual es: {saldo}")
+        
+    if saldo - transaccion < 0:
+        print("Tu saldo es insuficiente")
 
-    if tipo == "i" or tipo == "I":
+#FUNCION PARA INGRESAR DINERO
+    elif tipo == "i" or tipo == "I":
         saldo = saldo + transaccion
-        transacciones.append([transaccion, tipo])
+        conteo = conteo + 1
+        tipo = "- Ingreso"
+        lista1.append([transaccion, tipo])
         print(f"Tu saldo actual es: {saldo}")
 
+#DAMOS UN INFORME DE LAS TRANSACCIONES HECHAS Y LA MEDIA
 if saldo == 0:
-    for t in transacciones:
-        print(f"Tu... {t[]}")
+    for i in lista1:
+        print (i[0], i[1])
+    
+    for i in lista1:
+        media = (media + i[0]) / conteo
+    
+    print (f"El total de transacciones hechas es: {conteo}")
+    print (f"La media de transacciones hechas es: {media}")
 
-        #for i in t:
-            #print(i, end="")
+
+
+   
+   
 
 
 
