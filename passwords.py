a48b7b0e1ea626f0675a43d72ea6166884342486
don-programon <125381068+don-programon@users.noreply.github.com>
2023-02-27 15:17:26 +0100

    Add files via upload

---
+++ passwords.py
@@ -0,0 +1,18 @@
+usuario = input("Introduce tu usuario: ")
+prohibido = ["password", "contraseña", "123456"]
+             
+while len(usuario) <= 5:
+    print("El usuario debe tener mas de 5 caracteres")
+    usuario = input("Introduce tu usuario: ")
+
+if len(usuario) > 5:
+    password = input("Introduce tu password: ")
+
+    while len(password) < 6:
+        print("El password debe tener mas de 6 caracteres")
+        password = input("Introduce tu password: ")
+    while password in prohibido:
+        print("El password no puede ser esa palabra")
+        password = input("Introduce tu password: ")
+
+
