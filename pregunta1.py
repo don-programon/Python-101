a5ca94f3b9ab7e66050daed96de2858bb217af82
don-programon <125381068+don-programon@users.noreply.github.com>
2023-04-19 16:48:32 +0200

    Add files via upload

---
+++ pregunta1.py
@@ -0,0 +1,48 @@
+
+# DEFINIMOS CLASE PRINCIPAL CON SUS ATRIBUTOS
+class Animal:
+    def __init__(self, nombre, patas):
+        self.nombre = nombre
+        self.patas = patas
+
+# DEFINIMOS CLASE PERRO QUE HEREDA DE ANIMAL CON SUS PROPIOS METODOS
+class Perro(Animal):
+    def __init__(self, nombre, patas, raza):
+        super().__init__(nombre, patas)
+        self.nombre = nombre
+        self.patas = patas
+        self.raza = raza
+
+    def ladrar(self):
+        print(f"{self.nombre} esta ladrando. WOOF! WOOF!")
+    
+    def correr(self):
+        print(f"{self.nombre} esta corriendo como loco!")
+
+# DEFINIMOS CLASE PAJARO QUE HEREDA DE ANIMAL CON SUS PROPIOS METODOS
+class Pajaro(Animal):
+    def __init__(self, nombre, patas):
+        super().__init__(nombre, patas)
+        self.nombre = nombre
+        self.patas = patas
+    
+    def tweet(self):
+        print(f"{self.nombre} esta haciendo ruido. TWEET! TWEET!")
+    
+    def volar(self):
+        print(f"{self.correr} esta volando hacia el infinito!")
+
+# EN LA APLICACION PRINCIPAL PEDIMOS AL USUARIO PARA INSTANCIAR UN PERRO
+if __name__ == "__main__":
+    perros = []
+    
+    nombre = input("Introducir un nombre de perro: ")
+    patas = input("Introducir patas: ")
+    raza = input("Introducir la raza: ")
+    perro = Perro(nombre, patas, raza)
+    perros.append(perro)
+    print(f"NOMBRE: {perro.nombre}, PATAS: {perro.patas}, RAZA: {perro.raza}")
+
+# LLAMAMOS A LOS METODOS DE PERRO PARA PROBAR QUE FUBNCIONAN
+    perro.ladrar()
+    perro.correr()
---
+++ pregunta2.py
@@ -0,0 +1,6 @@
+lista = [5, 3, 12, -6, -3, 1, 6, 8, -12]
+
+#CREAMOS UN BUCLE SOBRE LA LISTA CON UNA CONDICION PARA EVITAR LOS NUMEROS NEGATIVOS
+for i in lista:
+    if i >= 0:
+        print(f"Hola soy el numero: {i}")
